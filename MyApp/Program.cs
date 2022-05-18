2de8f91d747bd2d2478d38a3e35627f35427182e
lucas5203 <lucasdanielbrandan0@gmail.com>
2022-05-18 11:44:45 -0300

    primer commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+if(5>3){
+    Console.WriteLine("5 es mayor que 3");
+}else{
+    Console.WriteLine("5 es menor que 3");
+}
+string num1 = Console.ReadLine();
+string num2 = Console.ReadLine();
+int resultado = Convert.ToInt32(num1)+Convert.ToInt32(num2);
+Console.WriteLine("La suma de "+num1+" + "+num2+" = "+resultado);
+
+int resultado2 = Convert.ToInt32(num1)-Convert.ToInt32(num2);
+Console.WriteLine("La resta de "+num1+" - "+num2+" = "+resultado2);
